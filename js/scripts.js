8033047e7d27d3ba16e0cbc4919c65c2a65d5d2f
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-05-09 16:02:14 -0400

    Mensaje de Alerta
    creacion de mensajes de alerta para  informar si se rellenaron todos los campos o si estan vacios.

--- js/scripts.js
+++ js/scripts.js
@@ -28,19 +28,92 @@
 // console.log(nuevoEnlace);
 
 //Seleccionar elementos y asociarles un evento
-const btnEnviar = document.querySelector('.boton--primario');
-btnEnviar.addEventListener('click', function(evento){
-    console.log(evento);
+// const btnEnviar = document.querySelector('.boton--primario');
+// btnEnviar.addEventListener('click', function(evento){
+//     console.log(evento);
+//     evento.preventDefault();
+
+//     //validar un formulario
+    
+//     console.log('enviando formulario');
+// });
+
+//----------------------------------------------------------------------
+//Eventos de los input y textarea
+const datos = { //Arreglo que permitira guardar datos de los input
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+//Esto permite seleccionar los input en especificos
+const nombre = document.querySelector('#nombre');  //Entre parentesis selecciona el id del input
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formulario');
+
+// nombre.addEventListener('input', function(e) {
+//     console.log(e.target.value);
+// });
+nombre.addEventListener('input', leerTexto);
+
+// email.addEventListener('input', function(e) {
+//     console.log(e.target.value);
+// });
+email.addEventListener('input', leerTexto);
+
+// mensaje.addEventListener('input', function(e) {
+//     console.log(e.target.value);
+// });
+mensaje.addEventListener('input', leerTexto);
+
+//El evento de Submit
+constformulario = document.querySelector('.formulario');
+formulario.addEventListener('submit', function(evento){
     evento.preventDefault();
 
-    //validar un formulario
+    //Validar el formulario
+
+    const { nombre, email, mensaje } = datos;
     
-    console.log('enviando formulario');
+    if(nombre === '' || email === '' || mensaje === ''){
+        mostrarError('Todos los campos son obligatorios');
+
+        return;
+    }
+
+    //Crear la alerta de Enviar correctamente
+    mostrarMensaje('Mensaje enviado correctamente');
 });
 
-//Eventos de los input y textarea
-const nombre = document.querySelector('#nombre');
+function leerTexto(e) {
+    //console.log(e.target.value);
+    datos[e.target.id] = e.target.value; //Esto permite almacenar los datos en el arreglo Datos
 
-nombre.addEventListener('input', function(e) {
-    console.log(e);
-});
+    // console.log(datos);
+}
+
+//Muestra un error en pantalla
+function mostrarError(mensaje) {
+    const error = document.createElement('P');
+    error.textContent = mensaje;
+    error.classList.add('error');
+
+    formulario.appendChild( error );
+
+    //Esto permitira que el mensaje desaparezca despues de 5 segundos
+    setTimeout(() => {
+        error.remove();
+    }, 5000);
+}
+//Muestra mensaje de enviado
+function mostrarMensaje(mensaje) {
+    const alerta = document.createElement('P');
+    alerta.textContent = mensaje;
+    alerta.classList.add('correcto');
+
+    formulario.appendChild( alerta );
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 5000);
+}
